30377fbc89c7ff6f353722d605a3018d225d7071
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2023-12-23 00:38:15 +0600

    router define

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', []);
+Route::post('/', []);
+
+Route::get('/dashboard', []);
+
+Route::get('/location', []);
+Route::post('/location', []);
+Route::delete('/location', []);
+
+Route::get('/seat-quality', []);
+Route::post('/seat-quality', []);
+Route::delete('/seat-quality', []);
+
+Route::get('/bus', []);
+Route::post('/bus', []);
+Route::put('/bus', []);
+Route::delete('/bus', []);
+
+Route::get('/trip', []);
+Route::post('/trip', []);
+Route::put('/trip', []);
+Route::delete('/trip', []);
